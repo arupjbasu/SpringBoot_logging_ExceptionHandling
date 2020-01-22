1d01bb4ea6545bb6a56bec9ade4a299aec19af0d
Arup Basu <abasu1@ra.rockwell.com>
2020-01-22 17:36:29 +0530

    added template

--- demo/src/main/java/com/arup/demo/DemoApplication.java
+++ demo/src/main/java/com/arup/demo/DemoApplication.java
@@ -8,12 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +30,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+   @Bean
+   public RestTemplate getRestTemplate() {
+      return new RestTemplate();
+   }
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
@@ -85,6 +94,45 @@ class Product {
  
         return "Howdy! Check out the Logs to see the output...";
     }
+}
+
+@RestController
+class ConsumeWebService {
+   @Autowired
+   RestTemplate restTemplate;
+
+   @RequestMapping(value = "/template/products")
+   public String getProductList() {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      HttpEntity<String> entity = new HttpEntity<String>(headers);
+      
+      return restTemplate.exchange("http://localhost:9090/products", HttpMethod.GET, entity, String.class).getBody();
+   }
+   @RequestMapping(value = "/template/products", method = RequestMethod.POST)
+   public String createProducts(@RequestBody Product product) {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      HttpEntity<Product> entity = new HttpEntity<Product>(product,headers);
+      
+      return restTemplate.exchange("http://localhost:9090/products", HttpMethod.POST, entity, String.class).getBody();
+   }
+   @RequestMapping(value = "/template/products/{id}", method = RequestMethod.PUT)
+   public String updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      HttpEntity<Product> entity = new HttpEntity<Product>(product,headers);
+      
+      return restTemplate.exchange( "http://localhost:9090/products/"+id, HttpMethod.PUT, entity, String.class).getBody();
+   }
+   @RequestMapping(value = "/template/products/{id}", method = RequestMethod.DELETE)
+   public String deleteProduct(@PathVariable("id") String id) {
+      HttpHeaders headers = new HttpHeaders();
+      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      HttpEntity<Product> entity = new HttpEntity<Product>(headers);
+      
+      return restTemplate.exchange("http://localhost:9090/products/"+id, HttpMethod.DELETE, entity, String.class).getBody();
+   }
 }
 
 @RestController
